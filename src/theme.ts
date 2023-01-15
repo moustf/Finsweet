4ec50cf945ecf6039f6cdbcb3ea81f2853b6aa8c
Mustafa Salem <mustafaissasalem@gmail.com>
2023-01-15 20:07:12 +0200

    feat: added the mui theme file
    Close: #4

---
+++ src/theme.ts
@@ -0,0 +1,18 @@
+import { createTheme } from '@mui/material';
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      light: '#FBF6EA',
+      main: '#FFD050;',
+      dark: '#EDC14A',
+      contrastText: '#232536',
+    },
+    secondary: {
+      light: '#F4F4F4',
+      main: '#232536',
+      dark: '#4C4C4C',
+      contrastText: '#ffffff',
+    },
+  },
+});
