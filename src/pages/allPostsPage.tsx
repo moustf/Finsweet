ea6194a149d6f44ee68022e1cc7209c148e3df9b
Mustafa Salem <mustafaissasalem@gmail.com>
2023-01-20 17:24:17 +0200

    feat: added all posts page route and created the page component with the header
    Close: #16

--- src/pages/allPostsPage.tsx
+++ src/pages/allPostsPage.tsx
@@ -1,5 +1,14 @@
 import { FC } from 'react';
+import { Box } from '@mui/material';
 
-export const dummy: FC = () => (
-  <div />
+import { Header } from '../components/Header';
+import { FeaturedPostSection } from '../components/allPostsPage/FeaturedPostSection';
+
+export const AllPostsPage: FC = () => (
+  <Box
+    component="main"
+  >
+    <Header />
+    <FeaturedPostSection />
+  </Box>
 );
