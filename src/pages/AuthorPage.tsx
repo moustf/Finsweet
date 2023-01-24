1d91e157fece38ff68240b87963ac9754d182480
Mustafa Salem <mustafaissasalem@gmail.com>
2023-01-24 20:49:06 +0200

    feat: created the author posts section and added footer section
    Close: #32

--- src/pages/AuthorPage.tsx
+++ src/pages/AuthorPage.tsx
@@ -1,7 +1,9 @@
 import { Box } from '@mui/material';
 import { FC } from 'react';
 import { IntroductionSection } from '../components/authorProfile/IntroductioinSection';
+import { PostsSection } from '../components/authorProfile/PostsSection';
 import { Header } from '../components/Header';
+import { Footer } from '../components/Footer';
 
 export const AuthorPage: FC = () => (
   <Box
@@ -9,5 +11,7 @@ export const AuthorPage: FC = () => (
   >
     <Header />
     <IntroductionSection />
+    <PostsSection />
+    <Footer page="author" />
   </Box>
 );
